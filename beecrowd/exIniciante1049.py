ad428c79e01b12bbab4d3d6ca6aa753fc20b08bb
Odair H <odair.sp@hotmail.com>
2022-06-14 09:08:25 -0300

    adicionado exIniciante1049-1052.py

--- beecrowd/exIniciante1049.py
+++ beecrowd/exIniciante1049.py
@@ -23,7 +23,7 @@ vertebrado              homem
 mamifero                
 onivoro                 
 
-vertebrado              aguia 
+vertebrado              aguia
 ave
 carnivoro
 
@@ -32,6 +32,29 @@ anelideo
 onivoro
 
 '''
-animais = []
+animal1 = input()
+animal2 = input()
+animal3 = input()
 
-animais[0][0][0]= "aguia"
+if animal1 == "vertebrado":
+    if animal2 == "ave":
+        if animal3 == "carnivoro":
+            print("aguia")
+        else:
+            print("pomba")
+    else:
+        if animal3 == "onivoro":
+            print("homem")
+        else:
+            print("vaca")
+else:
+    if animal2 == "inseto":
+        if animal3 == "hematofago":
+            print("pulga")
+        else:
+            print("lagarta")
+    else:
+        if animal3 == "hematofago":
+            print("sanguessuga")
+        else:
+            print("minhoca")
