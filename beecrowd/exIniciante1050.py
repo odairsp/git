71735fcae2c8c3ae90ac7e84cf1df3e3f4828f2e
Odair H <odair.sp@hotmail.com>
2022-06-14 09:09:08 -0300

    adicionado exIniciante1049-1052.py

---
+++ beecrowd/exIniciante1050.py
@@ -0,0 +1,31 @@
+''' 
+DDD
+Adaptado por Neilor Tonin, URI  Brasil
+
+Timelimit: 1
+Leia um número inteiro que representa um código de DDD para discagem interurbana. Em seguida, informe à qual cidade o DDD pertence, considerando a tabela abaixo:
+
+
+
+
+Se a entrada for qualquer outro DDD que não esteja presente na tabela acima, o programa deverá informar:
+DDD nao cadastrado
+
+Entrada
+A entrada consiste de um único valor inteiro.
+
+Saída
+Imprima o nome da cidade correspondente ao DDD existente na entrada. Imprima DDD nao cadastrado caso não existir DDD correspondente ao número digitado.
+
+Exemplo de Entrada	Exemplo de Saída
+11                  Sao Paulo
+'''
+
+DDD = {61: "Brasilia", 71: "Salvador", 11: "Sao Paulo",21: "Rio de Janeiro",
+       32: "Juiz de Fora", 19: "Campinas", 27: "Vitoria", 31: "Belo Horizonte"}
+
+escolha = int(input())
+if escolha in DDD:
+    print(DDD[escolha])
+else:
+    print("DDD nao cadastrado")
